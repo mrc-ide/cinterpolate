b6015a6b10267555e57b707df4df2f2ae355b638
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-07-09 08:46:19 +0100

    Make compatible with C++

--- inst/include/cinterpolate/cinterpolate.h
+++ inst/include/cinterpolate/cinterpolate.h
@@ -1,5 +1,10 @@
-#ifndef _CINTERPOLATE_H_
-#define _CINTERPOLATE_H_
+#ifndef CINTERPOLTE_CINTERPOLATE_H_
+#define CINTERPOLTE_CINTERPOLATE_H_
+
+// Allow use from C++
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stddef.h> // size_t
 
@@ -7,5 +12,9 @@ void *cinterpolate_alloc(const char *type, size_t n, size_t ny,
                          double *x, double *y);
 int cinterpolate_eval(double x, void *obj, double *y);
 void cinterpolate_free(void *obj);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
